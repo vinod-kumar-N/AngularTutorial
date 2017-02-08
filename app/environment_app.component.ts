400a03fb4b8da26710acb508191614c170fa0024
Vinodkumar N <24507@ITCINFOTECH.COM>
2017-02-08 16:00:02 +0530

    Angular fist push

---
+++ app/environment_app.component.ts
@@ -0,0 +1,13 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'my-app'
+})
+
+@View({
+  templateUrl: 'app/views/header.html'
+})
+
+export class AppComponent {
+  name: 'Vinod '
+}
---
+++ app/header.ts
@@ -0,0 +1,13 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'header'
+})
+
+@View({
+  template: '<h2>My Header is here</h2>'
+})
+
+export class AppComponent {
+
+}
